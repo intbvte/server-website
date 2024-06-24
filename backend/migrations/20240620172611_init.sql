c5c8cb5fe7bb0fcac12815e2f18e975a5ab1afe8
IThundxr <me@ithundxr.dev>
2024-06-24 12:22:25 -0400

    fix mc uuid size

--- backend/migrations/20240620172611_init.sql
+++ backend/migrations/20240620172611_init.sql
@@ -1,19 +1,18 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    discord_id         BIGINT PRIMARY KEY                                 NOT NULL,
-    discord_username   TEXT                                               NOT NULL,
-    -- fixme make sure ids are dashed or set it to 32 if they arent
-    minecraft_uuid VARCHAR(36),
-    created_at         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    last_updated       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    is_admin           BOOLEAN                  DEFAULT FALSE             NOT NULL
+    discord_id       BIGINT PRIMARY KEY                                 NOT NULL,
+    discord_username TEXT                                               NOT NULL,
+    minecraft_uuid   VARCHAR(36),
+    created_at       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    last_updated     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    is_admin         BOOLEAN                  DEFAULT FALSE             NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS sessions
 (
     id            SERIAL PRIMARY KEY       NOT NULL,
     user_id       BIGINT                   NOT NULL UNIQUE,
-    session_id    TEXT                   NOT NULL,
+    session_id    TEXT                     NOT NULL,
     access_token  TEXT                     NOT NULL,
     refresh_token TEXT                     NOT NULL,
     expires_at    TIMESTAMP WITH TIME ZONE NOT NULL,
