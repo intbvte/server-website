a74bfab5e661b94b928484738aa2bda43777dec7
Szedann <65607498+Szedann@users.noreply.github.com>
2024-06-24 18:48:42 +0200

    frontend aaaaaa

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,4 @@
-<script lang="ts">
-	let a = 'bees';
-	console.log(a);
-	a += 2 + 2;
-	console.log(a);
-</script>
-
-<h1 class="text-xl">Hello</h1>
-
-<img src="favicon.png" alt="" />
+<main class="max-w-screen-lg w-full mx-auto flex items-center flex-col my-10">
+	<h1 class="text-3xl">Steam 'n' Rails</h1>
+	<h2 class="text-xl">SMP Season 2</h2>
+</main>
---
+++ frontend/src/routes/whitelist/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	const backendUrl = 'http://localhost:8000/backend/';
+</script>
+
+<form action={backendUrl + 'minecraft/username/change'} method="post">
+	<input type="text" name="username" />
+	<button type="submit">add to whitelist</button>
+</form>
