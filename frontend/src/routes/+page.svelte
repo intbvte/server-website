6854230d34185b2c574921f9ec093d87fd5d2039
Szedann <65607498+Szedann@users.noreply.github.com>
2024-09-15 21:35:46 +0200

    whitelist after release

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,33 +1,11 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import { backendUrl } from '$lib/data';
+	import Whitelist from '$lib/Whitelist.svelte';
+import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	let username:string;
-	let usernameInput:string;
 
-	const openModal = () => {
-		if(!usernameInput) return
-		username = usernameInput
-		if(!dialog.open) dialog.showModal();
-	}
-	let dialog:HTMLDialogElement;
-
-	const submitToWhitelist = async () => {
-		await fetch(`${backendUrl}/minecraft/username/change`, {
-			method: "POST",
-			credentials: "include",
-			headers: {
-				"Content-Type": "application/x-www-form-urlencoded",
-			},
-			body: `username=${username}`,
-			mode: "no-cors"
-		})
-		window.location.reload();
-	}
-
-	const releaseDate = new Date(1727090020000)
+	const releaseDate = new Date(172815480000)
 	let remainingTime = releaseDate.valueOf() - Date.now().valueOf();
 	const getRemainingTimeString = () => {
 		let rts = remainingTime;
@@ -54,29 +32,8 @@
 	<img src="title.png" alt="Steam 'n' Rails SMP Season 2" class="max-w-2xl w-full px-2" />
 	<div class="grid grid-cols-2 gap-3 w-full max-w-sm drop-shadow-xl shadow-black">
 		<div class="col-span-2">
-			{#if data.user && !data.user.minecraft_uuid}
-				<div
-					class="flex flex-col items-center"
-				>
-					<h3 class="w-full text-center bg-gold pixelated text-lg text-darkred">
-						Get Whitelisted
-					</h3>
-					<div class="p-3 flex bg-gray pixelated w-full gap-1">
-						<input
-							type="text"
-							class="bg-input pixelated px-1 text-white outline-none border-8 placeholder-white w-full block placeholder:text-lightgray"
-							name="username"
-							placeholder="Minecraft username"
-							autocomplete="off"
-							spellcheck="false"
-							bind:value={usernameInput}
-							on:keyup={e=>{if(e.key == "Enter") openModal()}}
-						/>
-						<button type="button" on:click={openModal} class="outline-none">
-							<img src="ui/next.png" class="pixelated" width="48px" alt="confirm" />
-						</button>
-					</div>
-				</div>
+			{#if data.user && !data.user.minecraft_uuid && remainingTime < 0}
+				<Whitelist/>
 			{:else if data.user}
 				<div
 					class="col-span-2 bg-info p-4 text-center border-12 text-black flex flex-col pixelated bg-input"
@@ -100,28 +57,3 @@
 	</div>
 </main>
 
-<dialog bind:this={dialog} class="max-w-sm w-full backdrop:bg-dither backdrop:opacity-90">
-	<!-- <form method="dialog"> -->
-		<h3 class="w-full text-center bg-gold pixelatedtext-lg text-darkred">
-			Get Whitelisted
-		</h3>
-		<div class="p-3 flex flex-col bg-gray pixelated w-full gap-1">
-			<div class="flex flex-row gap-1">
-				<input
-					type="text"
-					class="bg-input pixelated px-1 text-white outline-none placeholder-white w-full text-shadow block placeholder:text-lightgray disabled:text-lightgray"
-					name="username"
-					placeholder="Minecraft username"
-					autocomplete="off"
-					spellcheck="false"
-					bind:value={usernameInput}
-					on:focusout={()=>username = usernameInput}
-					on:keyup={e=>{if(e.key == "Enter") username = usernameInput}}
-					autofocus
-				/>
-			</div>
-			<img src={`https://vzge.me/frontfull/832/${username}.png`} alt={`player skin of ${username}`} class="bg-dark p-4">
-			<input type="button" class="bg-button text-white p-2 text-center pixelated cursor-pointer" value="Add to whitelist" on:click={submitToWhitelist}>
-		</div>
-	<!-- </form> -->
-</dialog>
