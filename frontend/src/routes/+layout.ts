b8da798c63f06e9e0300237d35530b409d36f02b
Szedann <65607498+Szedann@users.noreply.github.com>
2024-09-17 18:49:30 +0200

    static site and proxy

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -2,11 +2,13 @@ import { userSchema } from '$lib/schemas';
 import { z } from 'zod';
 import { backendUrl } from '$lib/data';
 
-export const ssr = true;
-export const prerender = true;
+export const ssr = false;
 
 export async function load({ fetch }) {
-	const req = fetch(backendUrl + '/users/@me');
+	const req = fetch(backendUrl + '/users/@me', {
+		credentials: "include",
+		mode: "same-origin"
+	});
 	req.catch(console.error);
 	const response = await req;
 	let user: z.infer<typeof userSchema> | null = null;
