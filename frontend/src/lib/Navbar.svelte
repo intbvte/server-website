91040ce42102620c95412131ebc4d6d9473d4996
IThundxr <me@ithundxr.dev>
2024-09-29 11:44:02 -0400

    switch to single href & vars

--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -3,13 +3,11 @@
 	import type { userSchema } from '$lib/schemas';
 	import { backendUrl } from '$lib/data';
 	export let user: z.infer<typeof userSchema> | null;
+
+	let action = user != null ? "logout" : "login";
+	let text = user != null ? "Out" : "In";
 </script>
 
 <nav class="p-2.5 text-black">
-	{#if user != null}
-		<!-- {user} -->
-		<a href={`${backendUrl}/logout/discord`} class="underline underline-offset-2 p-0.5 bg-gray">Sign Out</a>
-	{:else}
-		<a href={`${backendUrl}/login/discord`} class="underline underline-offset-2 p-0.5 bg-gray">Sign In</a>
-	{/if}
+	<a href={`${backendUrl}/${action}/discord`} class="underline underline-offset-2 p-0.5 bg-gray">Sign {text}</a>
 </nav>
