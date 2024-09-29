25164b90f784367f06b120693e872d428e6ad402
IThundxr <me@ithundxr.dev>
2024-09-29 11:38:07 -0400

    BG for sign in/out button

--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -5,11 +5,11 @@
 	export let user: z.infer<typeof userSchema> | null;
 </script>
 
-<nav class="py-1 px-2 text-white">
+<nav class="py-1 px-2 text-black">
 	{#if user != null}
 		<!-- {user} -->
-		<a href={`${backendUrl}/logout/discord`} class="underline underline-offset-2">Sign Out</a>
+		<a href={`${backendUrl}/logout/discord`} class="underline underline-offset-2 bg-gray">Sign Out</a>
 	{:else}
-		<a href={`${backendUrl}/login/discord`} class="underline underline-offset-2">Sign In</a>
+		<a href={`${backendUrl}/login/discord`} class="underline underline-offset-2 bg-gray">Sign In</a>
 	{/if}
 </nav>
